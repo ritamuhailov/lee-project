3e574450fc5b665a7cd73be601bad9340402b020
ritamuhailov <ritamuhailov@gmail.com>
2022-08-01 13:40:14 +0300

    deploy

--- pages/api/project/index.js
+++ pages/api/project/index.js
@@ -1,5 +1,7 @@
+import App from 'next/app';
 import mongodb from '../../../middleware/mongodb';
 import Project from '../../../models/project';
+import task from '../task';
 
 const project = async (req, res) => {
     const { method } = req;
@@ -16,6 +18,26 @@ const project = async (req, res) => {
             console.log(error);
         }
     };
+
+    // if (method === "GET") {
+    //     try {
+    //         const project = await Project.find().populate(task);
+    //         res.status(200).json({ data: project })
+    //     } catch (error) {
+    //         res.status(500).json({ message: 'Internal Server Error' });
+    //         console.log(error);
+    //     }
+    // } 
+
+    // App.get("/project/:projectId", async (req, res) => {
+    //     const { projectId } = req.params;
+    //     try {
+    //         const retrievedTask = await project.findById(productId).populate("_id");
+    //         res.send(retrievedTask);
+    //     } catch (e) {
+    //         res.send(e);
+    //     }
+    // });
 }
 
 export default mongodb(project);
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,82 +1,82 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../users/Style.module.css'
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button'; 
-import { useState } from 'react';
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '../users/Style.module.css'
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import TextField from '@mui/material/TextField';
+// import Stack from '@mui/material/Stack';
+// import Button from '@mui/material/Button'; 
+// import { useState } from 'react';
 
 
-function Users() { 
+// function Users() { 
 
-    const [input, setInput] = useState({
-        email: "",
-        userName: "",
-        password: "" 
-    });
+//     const [input, setInput] = useState({
+//         email: "",
+//         userName: "",
+//         password: "" 
+//     });
 
-    const handleChange = (e, property) => {
-        setInput({
-            ...input,
-            [property]: e.target.value
-        })
-    }
-    console.log("input", input); 
+//     const handleChange = (e, property) => {
+//         setInput({
+//             ...input,
+//             [property]: e.target.value
+//         })
+//     }
+//     console.log("input", input); 
 
-    const [user, setUser] = useState([]);
+//     const [user, setUser] = useState([]);
 
-    const handleSubmit = (e) => {
-        setUser({user}); 
-        // setOpen(false);
-        fetch('api/user', {
-            method: 'POST',
-            body: JSON.stringify(input),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .catch(e => console.log(e))
-    }
-    return (
-        <Box className={styles.connectUser}
-            component="form"
-            sx={{
-                '& > :not(style)': { m: 1, width: '25ch' }
-            }}
-            noValidate
-            autoComplete="off">
-            <TextField 
-            label="Email" 
-            color="secondary" 
-            focused placeholder="אימייל"
-            value={input.email}
-            onChange={e => handleChange(e, "email")}  />
-            <TextField 
-            label="User Name" 
-            color="secondary" 
-            focused placeholder="שם משתמש"
-            value={input.userName}
-            onChange={e => handleChange(e, "userName")} />
-            <TextField 
-            label="Password" 
-            color="secondary" 
-            focused placeholder="סיסמא" 
-            value={input.password}
-            onChange={e => handleChange(e, "password")}
-            multiline />
-            <div>
-                <Button classname={styles.connectButton} onClick={handleSubmit} variant="contained">התחבר</Button>
-                {/* <Button variant="outlined">התחבר</Button> */}
-            </div>
-        </Box>
-    );
-}
+//     const handleSubmit = (e) => {
+//         setUser({user}); 
+//         // setOpen(false);
+//         fetch('api/user', {
+//             method: 'POST',
+//             body: JSON.stringify(input),
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//         })
+//             .then(res => res.json())
+//             .then(data => console.log(data))
+//             .catch(e => console.log(e))
+//     }
+//     return (
+//         <Box className={styles.connectUser}
+//             component="form"
+//             sx={{
+//                 '& > :not(style)': { m: 1, width: '25ch' }
+//             }}
+//             noValidate
+//             autoComplete="off">
+//             <TextField 
+//             label="Email" 
+//             color="secondary" 
+//             focused placeholder="אימייל"
+//             value={input.email}
+//             onChange={e => handleChange(e, "email")}  />
+//             <TextField 
+//             label="User Name" 
+//             color="secondary" 
+//             focused placeholder="שם משתמש"
+//             value={input.userName}
+//             onChange={e => handleChange(e, "userName")} />
+//             <TextField 
+//             label="Password" 
+//             color="secondary" 
+//             focused placeholder="סיסמא" 
+//             value={input.password}
+//             onChange={e => handleChange(e, "password")}
+//             multiline />
+//             <div>
+//                 <Button classname={styles.connectButton} onClick={handleSubmit} variant="contained">התחבר</Button>
+//                 {/* <Button variant="outlined">התחבר</Button> */}
+//             </div>
+//         </Box>
+//     );
+// }
 
-export default Users;
+// export default Users;
 
 
                 // import { useSyncExternalStore } from 'react';
