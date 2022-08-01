4e81c45abfa4dc675c5d3d3453f43fe26be455ed
ritamuhailov <ritamuhailov@gmail.com>
2022-08-01 15:47:41 +0300

    deploy 10

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,6 +1,6 @@
-import style from "../Header/Header.module.css"
+// import style from "../Header/Header.module.css"
 import React from 'react';
-import Link from 'next/link'
+// import Link from 'next/link'
 import Navbar from "../Navbar/Navbar";
 
 function Header() {
